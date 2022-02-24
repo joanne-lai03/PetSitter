27accee9ded26e758b8d37f3b859883b8a7973af
jinny <vedaforest@gmail.com>
2022-02-25 11:43:45 +1300

    Route basic settings

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -2,10 +2,13 @@ import React from 'react'
 import { useAuth0 } from '@auth0/auth0-react'
 import { cacheUser } from '../auth0-utils'
 import { Routes, Route } from 'react-router-dom'
+
+// 🎈components linked
+// import Registration from './Registration'
 import Nav from './Nav'
 import Main from './Main'
-
-// import Registration from './Registration'
+import PetsitterForm from './PetsitterForm'
+import PetsitterProfiles from './PetsitterProfiles'
 import Register from './Registration'
 
 
@@ -19,9 +22,10 @@ export default function App() {
         <Routes>
           <Route path='/' element={<Main />} />
           <Route path='/register' element={<Register />} />
-          <Route path='/profiles' element={<Profile />} />
+          {/* <Route path='/profiles' element={<Profile />} /> */}
           {/* 🎈 if you build your Componets comments out! this and check it's working */}
-          {/* <Route path='/petsitters/listing' element={<Listing />} /> */}
+          {/* <Route path='/petsitters' element={<Listing />} /> */}
+          <Route path='/petsitters/add' element={<PetsitterForm />} />
           <Route path='/petsitters/profiles' element={<PetsitterProfiles />} />
         </Routes>
       </div>
