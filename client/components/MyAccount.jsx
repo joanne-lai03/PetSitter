8023a60dbcb077c478e96e4bd36f467e462b2ac2
jinny <vedaforest@gmail.com>
2022-02-28 10:22:13 +1300

    icon

--- client/components/MyAccount.jsx
+++ client/components/MyAccount.jsx
@@ -1,15 +1,16 @@
 import React from 'react'
 
-export default function MyAccount(props) {
-  const { name, email, location } = props.user
+export default function MyAccount (props) {
+  const { name, email, location, description } = props.user
 
   return (
     <>
-      <h1>User Account</h1>
-      <ul className='list-primary'>
-        <li>name : {name}</li>
-        <li>email : {email}</li>
-        <li>location :{location}</li>
+      <h1>My Account</h1>
+      <ul className='myaccount-list'>
+        <li><i className="fa-solid icon_deco fa-user"></i> name : {name}</li>
+        <li><i className="fa-solid icon_deco fa-envelope"></i> email : {email}</li>
+        <li><i className="fa-solid icon_deco fa-location-dot"></i>location :{location}</li>
+        <li><i className="fa-solid icon_deco fa-kiwi-bird"></i>introduce : {description} </li>
       </ul>
 
     </>
