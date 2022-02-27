1055fa8940da1623b77e4e48b3f6decf810ab99f
cl-lin <clin3105@gmail.com>
2022-02-27 17:18:47 +1300

    Listings to listings

--- client/apis/listings.js
+++ client/apis/listings.js
@@ -3,7 +3,7 @@ import request from 'superagent'
 // router.get in petsitters
 export function getListing () {
   return request
-    .get('/api/v1/petsitters')
+    .get('api/v1/petsitters')
     .then(res => {
       console.log('from api', res.body)
       return res.body
@@ -34,7 +34,7 @@ export function postListing (listing) {
   console.log('this is newListing api', newListing)
 
   return request
-    .post('/api/v1/petsitters')
+    .post('api/v1/petsitters')
     .send(newListing)
     .then(res => res.body)
 }
@@ -42,7 +42,7 @@ export function postListing (listing) {
 // dlelet listing
 export function deleteListing (id) {
   return request
-    .delete('/api/v1/petsitters')
+    .delete('api/v1/petsitters')
     .send({ id })
     .then(res => res.body)
 }
