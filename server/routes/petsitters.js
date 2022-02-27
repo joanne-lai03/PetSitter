e8012ab8817b7b6b5cd5967c69ae4c7fa3cf287a
joanne-lai03 <joanne.lai92@gmail.com>
2022-02-27 17:11:49 +1300

    set up new branch for backend auth0

--- server/routes/petsitters.js
+++ server/routes/petsitters.js
@@ -1,12 +1,12 @@
 const express = require('express')
 const router = express.Router()
-
+const checkJwt = require('../auth0')
 const db = require('../db/petsitters')
 
 module.exports = router
 
 // Get listings for petsitters
-router.get('/', (req, res) => {
+router.get('/', checkJwt, (req, res) => {
   db.listPetsitters()
     .then((petsitters) => {
       res.json(petsitters)
@@ -18,7 +18,7 @@ router.get('/', (req, res) => {
 })
 
 // Add new petsitters lising
-router.post('/', (req, res) => {
+router.post('/', checkJwt, (req, res) => {
   const { name, location, petNumber, petType, petSize, homeType, serviceRate, availability, description, promoListing } = req.body
   const listingData = { name, location, petNumber, petType, petSize, homeType, serviceRate, availability, description, promoListing }
   db.addListing(listingData)
@@ -33,7 +33,7 @@ router.post('/', (req, res) => {
 })
 
 // Delete a single petsitters listing
-router.delete('/', (req, res) => {
+router.delete('/:id', checkJwt, (req, res) => {
   const { id } = req.body
   db.deleteListing(id)
     .then(() => {
