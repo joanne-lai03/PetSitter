7c16722f30b0b864c5b92f9610355c3d96c20ed8
cl-lin <clin3105@gmail.com>
2022-02-28 12:09:22 +1300

    update mock data

--- server/db/seeds/petsitters.js
+++ server/db/seeds/petsitters.js
@@ -13,9 +13,9 @@ exports.seed = function (knex) {
           pet_type: 'cat',
           pet_size: 'small',
           home_type: 'house',
-          service_rate: '$20/day',
+          service_rate: 'Pet Sitting - $15 per hour',
           availability: 'All day weekends',
-          description: 'Hi, my name is Claire and I am a proud owner of two cats. I am located in the eastern suburbs etc',
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
           promo_listing: 'Cat owner who is happy to look after other cats'
         },
 
@@ -28,10 +28,10 @@ exports.seed = function (knex) {
           pet_type: 'dog',
           pet_size: 'small to medium',
           home_type: 'house',
-          service_rate: '$30/day',
+          service_rate: 'Pet Sitting - $15 per hour',
           availability: 'Thursday - Sunday from 5pm',
-          description: 'Dog lover who does not own her own pets but love looking after other dogs. Has plenty of experience looking after other dogs.',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Dog lover who does not own her own pets but love looking after other dogs. Has plenty of experience looking after other dogs.'
         },
 
         {
@@ -43,10 +43,10 @@ exports.seed = function (knex) {
           pet_type: 'cat',
           pet_size: 'small',
           home_type: 'granny flat',
-          service_rate: '$40/day',
+          service_rate: 'Pet Walking - $10 per hour',
           availability: 'Monday - Friday from 5pm',
-          description: 'Cat lover who is always happy to help take care of others cats',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Cat lover who is always happy to help take care of others cats'
         },
         {
           id: 4,
@@ -57,10 +57,10 @@ exports.seed = function (knex) {
           pet_type: 'cat',
           pet_size: 'small',
           home_type: 'apartment',
-          service_rate: '$30/day',
+          service_rate: 'Pet Walking - $10 per hour',
           availability: 'Weekends after 3pm',
-          description: 'Big fan of all pets and has previous experience owning a dog and a cat',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Big fan of all pets and has previous experience owning a dog and a cat'
         },
         {
           id: 5,
@@ -71,10 +71,10 @@ exports.seed = function (knex) {
           pet_type: 'dog',
           pet_size: 'medium to large',
           home_type: 'house',
-          service_rate: '$40/day',
+          service_rate: 'Pet Boarding - $40 per day',
           availability: 'Monday - Friday from 6pm',
-          description: 'Love having a dog run around my big backyard. Hoping to own my own dog one day!',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Love having a dog run around my big backyard. Hoping to own my own dog one day!'
         },
         {
           id: 6,
@@ -85,10 +85,10 @@ exports.seed = function (knex) {
           pet_type: 'dog',
           pet_size: 'medium to large',
           home_type: 'house',
-          service_rate: '$50/day',
+          service_rate: 'Pet Grooming - discuss with pet sitter',
           availability: 'Weekends from 5am - 8pm',
-          description: 'I love dogs!!! Woohoo!',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'I love dogs!!! Woohoo!'
         },
         {
           id: 7,
@@ -99,10 +99,10 @@ exports.seed = function (knex) {
           pet_type: 'dog',
           pet_size: 'small to medium',
           home_type: 'house',
-          service_rate: '$30/day',
+          service_rate: 'Pet Walking - $10 per hour',
           availability: 'Weekdays between 6pm - 9pm',
-          description: 'Dogs are sick! Yeah, they\'re mean.',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Dogs are sick! Yeah, they\'re mean.'
         },
         {
           id: 8,
@@ -113,10 +113,10 @@ exports.seed = function (knex) {
           pet_type: 'dog',
           pet_size: 'small to medium',
           home_type: 'house',
-          service_rate: '$40/day',
+          service_rate: 'Pet Boarding - $40 per day',
           availability: 'All day, everday',
-          description: 'I only look after dogs named Jerry',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'I only look after dogs named Jerry'
         },
         {
           id: 9,
@@ -127,10 +127,10 @@ exports.seed = function (knex) {
           pet_type: 'cat',
           pet_size: 'any size',
           home_type: 'flat',
-          service_rate: '$20/day',
+          service_rate: 'Pet Sitting - $15 per hour',
           availability: 'Weekends from 3pm',
-          description: 'Love animals of all sizes. The bigger the better!',
-          promo_listing: '????'
+          description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+          promo_listing: 'Love animals of all sizes. The bigger the better!'
         }
 
       ])
