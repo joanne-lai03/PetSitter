1865b8632e62393b301b373fc1b30ffd7d7782d6
joanne-lai03 <joanne.lai92@gmail.com>
2022-02-27 17:16:03 +1300

    fix server auth0

--- server/auth0.js
+++ server/auth0.js
@@ -4,54 +4,6 @@ const jwks = require('jwks-rsa')
 const domain = 'https://harakeke-2022-jinny.au.auth0.com'
 const audience = 'https://petsitter/api'
 
-<<<<<<< HEAD
-||||||| 600e0a3
-const getUserRoles = async (uid) => {
-  const accessToken = await getAccessToken()
-  const { body } = await request(`${domain}/api/v2/users/${uid}/roles`)
-    .set({ authorization: `Bearer ${accessToken}` })
-
-  return body[0]?.name
-}
-
-const getAccessToken = async () => {
-  const data = {
-    grant_type: 'client_credentials',
-    client_id: machine2machineClientId,
-    client_secret: machine2machineSecret,
-    audience: `${domain}/api/v2/`
-  }
-
-  const { body } = await request.post(`${domain}/oauth/token`)
-    .send(data)
-    .type('form')
-  return body.access_token
-}
-=======
-const getUserRoles = async (uid) => {
-  const accessToken = await getAccessToken()
-  const { body } = await request(`${domain}/api/v2/users/${uid}/roles`)
-    .set({ authorization: `Bearer ${accessToken}` })
-
-  return body[0]?.name
-}
-
-const getAccessToken = async () => {
-  const data = {
-    grant_type: 'client_credentials',
-    client_id: machine2machineClientId,
-    client_secret: machine2machineSecret,
-    audience: `${domain}/api/v2/`
-  }
-
-  const { body } = await request.post(`${domain}/oauth/token`)
-    .send(data)
-    .type('form')
-  return body.access_token
-}
-
-
->>>>>>> 2748135d598b2943496bc3bbc10ec0b8979f3649
 const checkJwt = jwt({
   secret: jwks.expressJwtSecret({
     cache: true,
